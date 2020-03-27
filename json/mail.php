db3c4ca8535426599951f3e8227797f7bff49f5b
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2020-03-27 21:17:36 +0100

    ajout de bcp trop de chose

--- json/mail.php
+++ json/mail.php
@@ -2,16 +2,16 @@
 
 require_once "DataBase.php";
 
-$id = getElement('Id',$pseudo,'identifiant','User');
-$dateEnvoi = getMail('dateEnvoi',$id);
-$heureEnvoi = getMail('dateEnvoi',$id);
-$content = getMail('Content',$id);
-
-
-$pseudo=$_SESSION['user'];
-$mail = getMail('email',$id);
-date_default_timezone_set('Europe/Paris');
-if ($heureEnvoi == date("Y-m-d") && $heureEnvoi == date('H:i'))
-    sendMail($mail,$content);
+//$id = getElement('Id',$pseudo,'identifiant','User');
+//$dateEnvoi = getMail('dateEnvoi',$id);
+//$heureEnvoi = getMail('dateEnvoi',$id);
+//$content = getMail('Content',$id);
+//
+//
+//$pseudo=$_SESSION['user'];
+//$mail = getMail('email',$id);
+//date_default_timezone_set('Europe/Paris');
+//if ($heureEnvoi == date("Y-m-d") && $heureEnvoi == date('H:i'))
+//    sendMail($mail,$content);
 deleteNote();
 
