f1ca92880004b11ee8ec54eb642f9806b34bff09
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2020-03-27 23:39:09 +0100

    le mailllllllll

--- json/DataBase.php
+++ json/DataBase.php
@@ -54,11 +54,10 @@ function sendMail($mail,$content) {
 
 function deleteNote() {
     $date =  date("Y-m-d" );
-    $heure = date('H:i');
 
-    $query =loadDb()->prepare('DELETE FROM note WHERE dateEnvoi < :date  OR dateEnvoi =:date AND heureEnvoi < :heure');
+
+    $query =loadDb()->prepare('DELETE FROM note WHERE dateEnvoi < :date');
     $query->bindValue(':date',$date,PDO::PARAM_STR);
-    $query->bindValue(':heure',$heure,PDO::PARAM_STR);
     $query->execute();
 }
 
