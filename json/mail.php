2a8f3e20cef2f3ee27a73288de97a7060cbb8534
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2020-03-27 16:42:26 +0100

    ajout de bcp trop de chose

--- json/mail.php
+++ json/mail.php
@@ -11,10 +11,8 @@ date_default_timezone_set('Europe/Paris');
 $date = htmlspecialchars($_POST['dates']);
 $note = htmlspecialchars($_POST['mNote']);
 $hour = htmlspecialchars($_POST['hour']);
+deleteNote();
 if($date == date("Y-m-d" ) && $hour == date('H:i')){
     sendMail($mail,$note);
-}
 
-else if($date <= date("Y-m-d" ) && $hour < date('H:i')){
-    deleteNote();
 }
