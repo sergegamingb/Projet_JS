a565b75dd7edfaf51f8eb41b4b459fed53a6d0ed
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2020-04-10 00:07:19 +0200

    supp note

--- js/seeNote.js
+++ js/seeNote.js
@@ -14,15 +14,15 @@
             for (let i in data.note) {
                 id =data.note[i]['Id'];
                 info = '<div class="col-3 d-flex justify-content-around"><div class="card">' +
-                    '<i   class="edit fa fa-times" id= ' + id + '></i>' +
-                    '<div class="card-body"> '+ data.note[i]['Content']+'</div>' +
-                    '<input   type="button" value="Modifier note" class="login_btn btn ">' +
+                    '<div class=" remove fa fa-times btn btn-danger" id= ' + id + ' ></div>' +
+                    '<div class="card-body content"> '+ data.note[i]['Content']+'</div>' +
+                    '<input type="button" value="Modifier note" class="login_btn btn ">' +
                     '</div></div>';
                 $('#note').append(info);
 
             }
         });
-        $(document).on('click','.edit',function (event) {
+        $(document).on('click','.remove',function (event) {
             let id = event.target.id;
             $.ajax({
                 url : 'json/resetNote.php?id='+id,
